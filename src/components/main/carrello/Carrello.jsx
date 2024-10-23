f59b60186b5bf7ca3015e4686bdc5fee7dde9b4b
Valeria Sozzi <valeriasozzi24@gmail.com>
2024-10-23 11:19:36 +0200

    add carrello completed

--- src/components/main/carrello/Carrello.jsx
+++ src/components/main/carrello/Carrello.jsx
@@ -1,17 +1,59 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
 import "./Carrello.scss";
 
 const Carrello = ({ cartElements }) => {
-  let sum = cartElements.reduce((total, el) => total + el.price, 0);
+  //creare un nuovo state dove appoggiarsi per il nuovo array
+  const [cartPizzas, setCartPizzas] = useState([]);
+  //creare la variabile di appoggio per la somma totale
+  let sum = cartPizzas.reduce((total, el) => total + el?.totalPrice, 0);
+
+  //creare uno useeffect per ciclare ogni volta cambia cartElements
+  useEffect(() => {
+    //creare il ciclo
+    cartElements.map((cartElement) => {
+      //se c'è già una pizza con lo stesso id nel carrello
+      if (cartPizzas.some((pizza) => cartElement.id === pizza.id)) {
+        //allora creo un nuovo array dove ciclare il nuovo array di appoggio
+        const newCartPizzas = cartPizzas.map((cartPizza) => {
+          //se c'è già la pizza con lo stesso id
+          if (cartPizza.id === cartElement.id) {
+            //trovata la pizza la ritorno con il prezzo totale aggiornato e la quantità
+            return {
+              ...cartPizza,
+              totalPrice: cartPizza.totalPrice + cartPizza.price,
+              quantity: cartPizza.quantity + 1,
+            };
+          } //altrimenti ritorno la pizza così com'è
+          else {
+            return cartPizza;
+          }
+        });
+        //setto il nuovo array di appoggio con la nuova array create
+        setCartPizzas(newCartPizzas);
+      } //altrimenti se non c'è la pizza con lo stesso id
+      else {
+        //setto per aggiungere una nuova pizza aggiungendo all'oggetto il prezzo totale e la quantià
+        setCartPizzas((prev) => [
+          ...prev,
+          { ...cartElement, totalPrice: cartElement.price, quantity: 1 },
+        ]);
+      }
+    });
+  }, [cartElements]);
+
   return (
     <div className="card-cart">
       <h1>Carrello</h1>
       <div className="cart-body">
         <div>
-          {cartElements.map((cartElement) => (
-            <div key={cartElement.id} className="cartElement">
-              <div className="cart-pizza-name">{cartElement.name}</div>
-              <div className="cart-pizza-price">€ {cartElement.price}</div>
+          {cartPizzas.map((cartPizza) => (
+            <div key={cartPizza.id} className="cartElement">
+              <div className="cart-pizza-name">{cartPizza.name}</div>
+              <div>{cartPizza.quantity}</div>
+              <div className="cart-pizza-price">
+                € {cartPizza.price.toFixed(2)}
+              </div>
             </div>
           ))}
         </div>
@@ -19,7 +61,7 @@ const Carrello = ({ cartElements }) => {
           <div className="hr"></div>
           <div className="tot">
             <span>Totale</span>
-            <span>€ {sum}</span>
+            <span>€ {sum?.toFixed(2)}</span>
           </div>
         </div>
       </div>
